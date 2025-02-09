10d138befc7c7159c3e3d9c93d2479832fa784c7
cho.jardin <cho.jardin@gmail.com>
2025-02-09 20:11:58 +0900

    [feat] 페이지 레이아웃 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
-    <div className='grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20'>
-      <main className='row-start-2 flex flex-col items-center gap-8 sm:items-start'>
-        <span>폰트 테스트</span>
-      </main>
+    <div>
+      <main className='absolute left-0 right-0 m-auto h-full w-full max-w-[600px] overflow-y-auto'></main>
     </div>
   );
 }
